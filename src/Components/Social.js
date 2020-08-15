76a95389b8ad1a3cda30cc84b2be7f199895eb3c
Beros <59673191+Panosmpero@users.noreply.github.com>
2020-08-15 21:56:29 +0100

    added codepen

--- src/Components/Social.js
+++ src/Components/Social.js
@@ -3,33 +3,46 @@ import React from "react";
 const Social = () => {
   return (
     <div className="social">
-      <a 
-       href="https://www.facebook.com/panos.mpero"
-       id="profile-link"
-       target="_blank"
-       rel="noopener noreferrer"
-       ><i className="fab fa-facebook-square"></i></a>    
-    <a 
-       href="https://github.com/Panosmpero"
-       id="profile-link"
-       target="_blank"   
-       rel="noopener noreferrer"   
-       ><i className="fab fa-github"></i
-      ></a>
-    <a 
-       href="https://www.freecodecamp.org/panosbero"
-       id="profile-link"
-       target="_blank"
-       rel="noopener noreferrer"
-       ><i className="fab fa-free-code-camp"></i
-      ></a>
-    <a 
-       href="https://www.linkedin.com/in/panagiotis-berovalis-18353619a/"
-       id="profile-link"
-       target="_blank"
-       rel="noopener noreferrer"
-       ><i className="fab fa-linkedin"></i
-      ></a>
+      <a
+        href="https://www.linkedin.com/in/panagiotis-berovalis-18353619a/"
+        id="profile-link"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i className="fab fa-linkedin"></i>
+      </a>
+      <a
+        href="https://github.com/Panosmpero"
+        id="profile-link"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i className="fab fa-github"></i>
+      </a>
+      <a
+        href="https://www.freecodecamp.org/panosbero"
+        id="profile-link"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i className="fab fa-free-code-camp"></i>
+      </a>
+      <a
+        href="https://codepen.io/panosmpero"
+        id="profile-link"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i className="fab fa-codepen"></i>
+      </a>
+      <a
+        href="https://www.facebook.com/panos.mpero"
+        id="profile-link"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i className="fab fa-facebook-square"></i>
+      </a>
     </div>
   );
 };
