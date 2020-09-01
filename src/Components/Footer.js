ab6e0d4e061e27f822cec64c3bfba59537909422
Beros <59673191+Panosmpero@users.noreply.github.com>
2020-09-01 12:56:54 +0100

    card fix

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="footer" id="footer">
       <div>copyright&copy;2020 bero all rights reserved</div>
-      <div>Updated: 12/08/2020</div>
+      <div>Updated: 01/09/2020</div>
     </div>
   )
 }
