4239e3eeb4d3c8e6a49f36f012b599a69420daab
Beros <59673191+Panosmpero@users.noreply.github.com>
2021-03-09 10:51:50 +0000

    cv update

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="footer" id="footer">
       <div>copyright&copy;2020 bero all rights reserved</div>
-      <div>Updated: 08/09/2020</div>
+      <div>Updated: 09/03/2021</div>
     </div>
   )
 }
